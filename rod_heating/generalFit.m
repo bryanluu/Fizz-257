4e22fa240f741eaad491cfa07fd808b1ec819916
Bryan Luu <bryanluu30794@gmail.com>
2014-07-04 13:50:19 -0700

    Added error display

--- rod_heating/generalFit.m
+++ rod_heating/generalFit.m
@@ -6,15 +6,12 @@ dt = endTime/timePoints;
 
 
 %======ADJUST PARAMETERS HERE======
-fitParams = {'emissivity'};
-guess = [0.1];
+fitParams = {'emissivity', 'hConvection', 'c'};
+guess = [0, 12.5, 900];
 % the program automagically adjusts number of fit variables and stuff
 
 
-% func = @(x) sum((subsref(getTemperatureGradient(endTime,dt,fitParams{1}, x(1),fitParams{2}, x(2),fitParams{3}, x(3)),struct('type','()','subs',{{1:10:50000,1}}))-T1(1:5000,:)).^2+...
-%                 (subsref(getTemperatureGradient(endTime,dt,fitParams{1}, x(1),fitParams{2},x(2),fitParams{3},x(3)),struct('type','()','subs',{{1:10:50000,17}}))-T2(1:5000,:)).^2+...
-%                 (subsref(getTemperatureGradient(endTime,dt,fitParams{1},x(1),fitParams{2},x(2),fitParams{3},x(3)),struct('type','()','subs',{{1:10:50000,33}}))-T3(1:5000,:)).^2);
-%             
+    
 errorVectorT1 = @(x) (getTemperatureVector(0, endTime,dt,fitParams, x) - T1);
 errorVectorT2 = @(x) (getTemperatureVector(0, endTime,dt,fitParams, x) - T2);
 errorVectorT3 = @(x) (getTemperatureVector(0, endTime,dt,fitParams, x) - T3);
@@ -23,7 +20,6 @@ errorLeastSquares = @(x) sum( errorVectorT1(x).^2 + ...
                                 errorVectorT2(x).^2 + ...
                                 errorVectorT3(x).^3);
             
-             %(subsref(getTemperatureGradient(1500,0.1,x(1),x(2),x(3)),struct('type','()','subs',{{1000:5:15000,17}}))-T2(200:3000,:)).^2+...
 
 
 
@@ -46,6 +42,8 @@ for paramNum = 1:length(fitParams)
     disp([fitParams{paramNum} ', with guess ' num2str(guess(paramNum))]);
 end
 
+disp(['Guess Error: ' num2str(errorLeastSquares(guess))]);
+
 start = tic;
 
 disp('...');
@@ -55,6 +53,8 @@ disp('...');
 for paramNum = 1:length(fitParams)
     disp([fitParams{paramNum} ': ' num2str(params(paramNum))]);
 end
+
+disp(['Fit Error: ' num2str(fval)]);
 
 disp(['Time Elapsed: ' num2str(toc(start))]);
 
@@ -67,7 +67,3 @@ plot(ElapsedTimeseconds, T3, 'g');
 title('Fitted Curve');
 
 
-% hold on;
-% plot(Time./2,T1,'k');
-% plot(Time./2,T2,'k');
-% plot(Time./2,T3,'k');
